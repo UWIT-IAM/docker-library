1c330fdefa0e1ab67189ea35d8c066f9786a8444
Justin Prosser <jprosser@uw.edu>
2022-10-04 19:46:58 -0700

    Updated the Poetry source URL from the janky raw.githubusercontent.com to install.python-poetry.org.

--- images/poetry/bootstrap-poetry-env.sh
+++ images/poetry/bootstrap-poetry-env.sh
@@ -2,7 +2,7 @@
 set -e
 
 BUILD_DEPS=('curl' 'build-essential')
-POETRY_INSTALL_URL=https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py
+POETRY_INSTALL_URL=https://install.python-poetry.org
 
 apt-get update
 apt-get install --no-install-recommends -y ${BUILD_DEPS[@]}
